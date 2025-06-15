2acb266182e9578619d781797a752620874274fe
lilacLunatic <8488221+lilacLunatic@users.noreply.github.com>
2025-06-15 20:01:41 -0300

    fix ZF boulders

--- soh/soh/Enhancements/randomizer/location_access/overworld/zoras_fountain.cpp
+++ soh/soh/Enhancements/randomizer/location_access/overworld/zoras_fountain.cpp
@@ -23,6 +23,9 @@ void RegionTable_Init_ZorasFountain() {
         LOCATION_NNL(RC_ZF_NEAR_JABU_POT_2,              logic->IsChild && logic->CanBreakPots()),
         LOCATION_NNL(RC_ZF_NEAR_JABU_POT_3,              logic->IsChild && logic->CanBreakPots()),
         LOCATION_NNL(RC_ZF_NEAR_JABU_POT_4,              logic->IsChild && logic->CanBreakPots()),
+        LOCATION_NNL(RC_ZF_BOULDER,                      logic->BlastOrSmash()),
+        LOCATION_NNL(RC_ZF_SILVER_BOULDER,               logic->CanUse(RG_SILVER_GAUNTLETS)),
+        LOCATION_NNL(RC_ZF_UNDERGROUND_BOULDER,          logic->CanUse(RG_SILVER_GAUNTLETS) && logic->BlastOrSmash()),
         LOCATION_NNL(RC_ZF_TREE,                         logic->IsChild && logic->CanBonkTrees()),
     }, {
         //Exits
